82f192b3a8fbfce628cd772fefd9cae09f4bde27
Jean Pierre <stick-master666@hotmail.com>
2021-02-02 09:54:06 -0500

    [add] react-helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -6,6 +6,8 @@ import '../styles/components/checkout.css';
 import AppContext from '../context/AppContext';
 //handleSumTotal
 import handleSumTotal from '../utils/handleSumTotal';
+//helmet
+import { Helmet } from 'react-helmet';
 
 const Checkout = () => {
     const { state, removeFromCart } = useContext(AppContext);
@@ -16,30 +18,35 @@ const Checkout = () => {
     };
 
     return (
-        <div className="checkout">
-            <div className="checkout-content">
-                {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos</h3>}
-                {cart.map((item) => (
-                    <div className="checkout-item">
-                        <div className="checkout-element">
-                            <h4>{item.title}</h4>
-                            <span>$ {item.price}</span>
+        <>
+            <Helmet>
+                <title>Lista de pedidos: Store online</title>
+            </Helmet>
+            <div className="checkout">
+                <div className="checkout-content">
+                    {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos</h3>}
+                    {cart.map((item) => (
+                        <div className="checkout-item">
+                            <div className="checkout-element">
+                                <h4>{item.title}</h4>
+                                <span>$ {item.price}</span>
+                            </div>
+                            <button type="button" onClick={handleRemove(item)}>
+                                <i className="fas fa-trash-alt"></i>
+                            </button>
                         </div>
-                        <button type="button" onClick={handleRemove(item)}>
-                            <i className="fas fa-trash-alt"></i>
-                        </button>
-                    </div>
-                ))}
-            </div>
-            {cart.length > 0 && (
-                <div className="checkout-sidebar">
-                    <h3>{`Precio Total: $ ${handleSumTotal(cart)}`}</h3>
-                    <Link to="/checkout/information">
-                        <button type="button">Continuar pedido</button>
-                    </Link>
+                    ))}
                 </div>
-            )}
-        </div>
+                {cart.length > 0 && (
+                    <div className="checkout-sidebar">
+                        <h3>{`Precio Total: $ ${handleSumTotal(cart)}`}</h3>
+                        <Link to="/checkout/information">
+                            <button type="button">Continuar pedido</button>
+                        </Link>
+                    </div>
+                )}
+            </div>
+        </>
     )
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,10 +3,38 @@ import React from 'react';
 import initialState from '../initialState';
 //components
 import Products from '../components/Products';
+//helmet
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
     return (
-        <Products products={initialState.products} />
+        <>
+            <Helmet>
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@JeanPiBot" />
+                <meta name="twitter:creator" content="@JeanPiBot" />
+                <meta name="twitter:title" content="Jean Pierre Giovanni" />
+                <meta name="twitter:description" content="Store online by Jp" />
+                <meta
+                    name="twitter:image"
+                    content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                />
+                <meta property="og:title" content="Platzi Conf Store" />
+                <meta property="og:description" content="Platzi Conf Store" />
+                <meta
+                    property="og:image"
+                    content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                />
+                <meta name="og:title" content="Jean Pierre Giovanni" />
+                <meta name="og:description" content="Blog de JbearP" />
+                <meta name="og:url" content="https://blog.jbearp.com" />
+                <meta name="og:site_name" content="Jean Pierre Giovanni" />
+                <meta name="og:locate" content="es_ES" />
+                <meta name="og:type" content="article" />
+                <meta name="fb:app_id" content="" />
+            </Helmet>
+            <Products products={initialState.products} />
+        </>
     )
 }
 
