cced7b2de156cd41bf45d4d0be694d59fb3b2378
Jean Pierre <stick-master666@hotmail.com>
2021-05-21 09:58:22 -0500

    [add] one mock

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,13 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+    const footer = mount(<Footer />);
+
     test('Render del componente footer', () => {
-        const footer = mount(<Footer />);
         expect(footer.length).toEqual(1);
+    });
+
+    test('Render el titulo', () => {
+        expect(footer.find('.footer-title').text()).toEqual('Store online by Jpbearp');
     });
 });
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../styles/components/footer.css';
 const Footer = () => {
     return (
         <div className="footer">
-            <p className="footer-title"> Store online by Jpbearp</p>
+            <p className="footer-title">Store online by Jpbearp</p>
             <p className="footer-copy">Todos los izquierdos reservados</p>
         </div>
     )
